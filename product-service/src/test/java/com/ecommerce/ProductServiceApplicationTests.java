8eaa6464fe0f2f7402b516ef59f82319a7a919b2
Anthony Viveros <anthonymvf09@gmail.com>
2026-07-14 13:28:30 -0400

    test: activate "test" profile in tests

--- product-service/src/test/java/com/ecommerce/ProductServiceApplicationTests.java
+++ product-service/src/test/java/com/ecommerce/ProductServiceApplicationTests.java
@@ -1,9 +1,13 @@
 package com.ecommerce;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ProductServiceApplicationTests {
 
     @Test
